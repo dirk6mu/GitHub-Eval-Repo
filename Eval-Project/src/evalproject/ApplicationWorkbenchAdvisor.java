4b216d36608fa2f8c71abc883e02ef1b6105cb6f
dirk6mu <dirk.mueller@piterion.com>
2011-11-07 13:37:31 +0100

    Initial Commit
    Change-Id: I0e6c17e0c5ed1ce85e4dc15016060243bf5c1071

---
+++ Eval-Project/src/evalproject/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package evalproject;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "Eval-Project.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
